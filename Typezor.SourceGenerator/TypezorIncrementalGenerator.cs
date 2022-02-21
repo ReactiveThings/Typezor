b3562bbc47fd6bb2babb9600f1cc02e067b002b8
ReactiveThings <office@ReactiveThings.com>
2022-02-21 21:26:42 +0100

    refactor for unit tests

--- Typezor.SourceGenerator/TypezorIncrementalGenerator.cs
+++ Typezor.SourceGenerator/TypezorIncrementalGenerator.cs
@@ -17,6 +17,8 @@ namespace Typezor.SourceGenerator
     [Generator]
     public class TypezorIncrementalGenerator : IIncrementalGenerator
     {
+        public Func<SourceProductionContext, ILogger, ITemplateOutput> TemplateOutputFactory { get; set; } = (context, logger) => new SourceProductionContextOutput(context, logger);
+
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
             var assemblyLoadContext = AssemblyLoadContextFactory.Create();
@@ -40,66 +42,66 @@ namespace Typezor.SourceGenerator
                 .Select(Compile(assemblyLoadContext))
                 .Where(p => p != null);
 
-            context.RegisterSourceOutput(razorTemplates.Combine(context.CompilationProvider.Combine(logging)), (c, pair) =>
+            context.RegisterSourceOutput(razorTemplates.Combine(context.CompilationProvider.Combine(logging)), (productionContext, pair) =>
             {
                 try
                 {
-                    ILogger logger = new SourceProductionContextLogger(c, pair.Right.Right);
-                    if (c.CancellationToken.IsCancellationRequested) return;
+                    var templateCompilation = pair.Left;
+                    foreach (var diagnostic in templateCompilation.Diagnostics)
+                    {
+                        productionContext.ReportDiagnostic(diagnostic);
+                    }
+
+                    if (pair.Left.Diagnostics.Any() || productionContext.CancellationToken.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    var logger = new SourceProductionContextLogger(productionContext, pair.Right.Right);
                     using (logger.Performance("RenderAsync"))
                     {
-                        var namespaceMetadata = new FileImpl(new RoslynGlobalNamespaceMetadata(pair.Right.Left.GlobalNamespace, new FindAllTypesVisitor()));
-                        var output = new SourceProductionContextOutput(c, logger);
-                        if (pair.Left.Diagnostics.Any())
-                        {
-                            foreach (var diagnostic in pair.Left.Diagnostics)
-                            {
-                                c.ReportDiagnostic(diagnostic);
-                            }
-                        }
-                        else
-                        {
-                            pair.Left.RenderAsync(namespaceMetadata, output, c.CancellationToken).Wait();
-                        }
+                        var output = TemplateOutputFactory(productionContext, logger);
+                        var namespaceMetadata = CreateNamespaceMetadata(pair.Right.Left);
+                        templateCompilation.RenderAsync(namespaceMetadata, output, productionContext.CancellationToken).Wait();
                     }
                 }
                 catch (Exception e)
                 {
-                    c.ReportDiagnostic(e.ToDiagnostic());
+                    productionContext.ReportDiagnostic(e.ToDiagnostic());
                 }
-
             });
         }
 
-        private static Func<AnalyzerConfigOptionsProvider, CancellationToken, bool> EmitLogging()
+        private static FileImpl CreateNamespaceMetadata(Compilation compilation)
         {
-            return (p,_) =>
-            {
-                bool emitLogging = false;
-                if (p.GlobalOptions.TryGetValue("typezor_emit_logging", out var emitLoggingSwitch))
-                {
-                    emitLogging = emitLoggingSwitch.Equals("true", StringComparison.OrdinalIgnoreCase);
-                }
-
-                return emitLogging;
-            };
+            return new FileImpl(new RoslynGlobalNamespaceMetadata(compilation.GlobalNamespace, new FindAllTypesVisitor()));
         }
 
-        private static Func<((string Path, string Text) Left, (ImmutableArray<Assembly> Left, ImmutableArray<string> Right) Right), CancellationToken, TemplateDescriptor> Compile(IAssemblyLoadContext assemblyLoadContext)
-        {
-            return (file, token) => TemplateDescriptor.Compile(
+        private static Func<AnalyzerConfigOptionsProvider, CancellationToken, bool> EmitLogging() =>
+            (p, _) =>
+                p.GlobalOptions.TryGetValue("typezor_info_as_warning", out var emitLoggingSwitch) &&
+                emitLoggingSwitch.Equals("true", StringComparison.OrdinalIgnoreCase);
+
+        private static Func<((string Path, string Text) Left, (ImmutableArray<Assembly> Left, ImmutableArray<string> Right) Right), CancellationToken, TemplateDescriptor> Compile(IAssemblyLoadContext assemblyLoadContext) =>
+            (file, token) => TemplateDescriptor.Compile(
                 file.Left.Text,
                 new ReferenceProvider(assemblyLoadContext).GetStandardAssemblies().Concat(file.Right.Left),
                 assemblyLoadContext,
                 file.Left.Path,
                 file.Right.Right,
                 token);
-        }
 
-        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, (string Path, string Text)> GetTextWithPath() => (p, token) => (p.Left.Path, Text: p.Left.GetText(token)?.ToString());
-        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, Assembly> LoadAssembly(IAssemblyLoadContext assemblyLoadContext) => (p, token) => assemblyLoadContext.LoadFromAssemblyPath(p.Left.Path);
+        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, (string Path, string Text)> GetTextWithPath() =>
+            (p, token) => (p.Left.Path, Text: p.Left.GetText(token)?.ToString());
+
+        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, Assembly> LoadAssembly(IAssemblyLoadContext assemblyLoadContext) =>
+            (p, _) => assemblyLoadContext.LoadFromAssemblyPath(p.Left.Path);
+
         private static Func<string, bool> IsNotNullOrWhiteSpace() => content => !string.IsNullOrWhiteSpace(content);
+
         private static Func<(string Path, string Text), bool> IsTemplateNotEmpty() => content => !string.IsNullOrWhiteSpace(content.Text);
-        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, string> GetText() => (p, token) => p.Left.GetText(token)?.ToString();
+
+        private static Func<(AdditionalText Left, AnalyzerConfigOptionsProvider Right), CancellationToken, string> GetText()
+            => (p, token) => p.Left.GetText(token)?.ToString();
     }
 }
