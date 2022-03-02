acbdac7feba91e2a634e7bc5a7f1a68b03eccb37
ReactiveThings <office@reactivethings.com>
2022-03-02 22:26:38 +0100

    add file scoped namespace

--- Typezor.Tests/CodeModel/Support/TypeInfo.cs
+++ Typezor.Tests/CodeModel/Support/TypeInfo.cs
@@ -2,18 +2,17 @@
 using Typezor.Tests.CodeModel.Support.Class;
 using Typezor.Tests.CodeModel.Support.Record;
 
-namespace Typezor.Tests.CodeModel.Support
-{
-    public class TypeInfo
-    {
-        public ClassInfo Class { get; set; }
-        public RecordInfo Record { get; set; }
-        public BaseClassInfo BaseClass { get; set; }
-        public GenericClassInfo<string> GenericClass { get; set; }
-        public InheritGenericClassInfo InheritGenericClass { get; set; }
+namespace Typezor.Tests.CodeModel.Support;
 
-        public string String { get; set; }
-        public ICollection<ClassInfo> ClassCollection { get; set; }
-        public ICollection<string> StringCollection { get; set; }
-    }
+public class TypeInfo
+{
+    public ClassInfo Class { get; set; }
+    public RecordInfo Record { get; set; }
+    public BaseClassInfo BaseClass { get; set; }
+    public GenericClassInfo<string> GenericClass { get; set; }
+    public InheritGenericClassInfo InheritGenericClass { get; set; }
+
+    public string String { get; set; }
+    public ICollection<ClassInfo> ClassCollection { get; set; }
+    public ICollection<string> StringCollection { get; set; }
 }
